e40c5d8b0f9d1bba5fea5615c076e0abbd5f2bce
Eric Miller <miller9428@yahoo.com>
2015-07-21 19:28:57 -0500

    Added the maps example.

---
+++ maps_example.php
@@ -0,0 +1,13 @@
+
+<img src="https://maps.googleapis.com/maps/api/staticmap?center=78702&zoom=14&size=400x400">
+
+<!-- Use a form and $_POST to display a map that's centered on 
+     the location a user enters. You can do this by plugging the
+     user's input into a Google Maps URL. -->
+
+<!-- Extra credit: Allow the user to control one of the extra options
+     from the Google Maps API docs (e.g. zoom, image size, etc) -->
+<!-- Try using one of the interesting input types, like check boxes,
+     radio buttons, or text areas. -->
+
+<!-- Maps API Docs: https://goo.gl/nRKIu -->
