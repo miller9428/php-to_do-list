235026cfb12edc7c384ab8eb83a5ac4f63535201
Eric Miller <miller9428@yahoo.com>
2015-07-23 20:31:56 -0500

    added files db_info and edit_post

--- edit_post.php
+++ edit_post.php
@@ -11,9 +11,20 @@ require_once('db_info.php');
 ?>
 
 
-<h1></h1>
+<h1>Create Post</h1>
 <form action="edit_post.php" method="POST">
-    <input type="text" name="address">
-    <input type="submit" name="submit">
+    <input type="text" name="title" placeholder="Create Post Title">
+    <input type="text" name="author" placeholder="Who are you?">
+    <textarea name="content" row="25" cols="80">Enter Text</textarea>
+    <input type="submit" name="submit" value="Add post">
 
 </form>
+
+<?php
+if (isset($_POST['submit'])) {
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+    $content = $_POST['content'];
+
+}
+?>
